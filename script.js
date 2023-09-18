77ae0737a936c53d85c5356c23bebd07d7bbdc4f
Regina Bonifacio <116869096+feiryrej@users.noreply.github.com>
2023-09-18 16:09:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+const enterElement = document.querySelector('.enter');
 
-function addTask(){
-    if(inputBox.value === ''){
-        alert("You must write something!");
+enterElement.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        addTask();
     }
-    else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
-    inputBox.value = "";
-}
+});
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-    }else if(e.target.tagName === "SPAN"){
+const listContainer = document.getElementById('list-container');
+
+listContainer.addEventListener('click', (e) => {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+        saveData();
+    } else if (e.target.tagName === 'SPAN') {
         e.target.parentElement.remove();
+        saveData();
     }
 }, false);
 
-function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
+const inputBox = document.getElementById('input-box');
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert('You must write something!');
+    } else {
+        let li = document.createElement('li');
+
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement('span');
+
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+
+        saveData();
+    }
+    inputBox.value = '';
 }
 
-function showtask(){
-    listContainer.innerHTML = localStorage.getItem("data");
+function saveData() {
+    localStorage.setItem('data', listContainer.innerHTML);
 }
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+
+showTask();
